817ed2a029856a5336546aa5351d332e42c6e9cf
Femsam10 <femis427@gmail.com>
2025-02-22 14:51:23 +0100

    first_backend_commmit

--- Video_10/index.php
+++ Video_10/index.php
@@ -8,33 +8,64 @@
 </head>
 <body>
     <?php
-    $tasks =[
-        "Laundry" => "Daniel",
-        "Trash" => "Frida",
-        "Vacuum" => "Basse",
-        "Dishes" => "Bella"
+     $food = [
+        "fruits" => array("apple", "banana", "cherry"),
+        "meat" => array("chicken", "fish", "beaf"),
+        "vegetables" => array("cucumber", "carrot"),
     ];
 
-    echo count($tasks)
+    echo $food["vegetables"][0];
 
-    //print_r($tasks);
 
-    //echo $tasks["Laundry"];
+
     /*
-    $fruits = [
-        "Apple", // 0
-        "Banana", // 1
-        "Cherry" // 2
+    $food = [
+        array("apple", "mango"),
+        "Banana",
+        "Cherry"
     ];
+
+    echo $food[0][1];
+    echo $food[1];
+
+    $fruits = ["Apple", "Banana", "Cherry"];
+    $test = ["Guava", "Strawberry"];
+
+    array_splice($fruits, 2, 0, "Mango");
+    array_splice($fruits, 3, 0, $test);
+    array_splice($fruits, 6, 0, "Orange");
+    print_r($fruits);
+    array_push($fruits, "Mango");
+    print_r($fruits);
+
+    $fruits[] = "Mango";
 
     // echo $fruits[1];
-    
     $fruits[1] = "Orange"; //Replacing data in an array
     $fruits[] = "Mango"; //Adding data to an array
     
     array_splice($fruits, 0, 1);
 
     echo $fruits[1];
+    
+    
+    
+   
+   $tasks =[
+        "Laundry" => "Daniel",
+        "Trash" => "Frida",
+        "Vacuum" => "Basse",
+        "Dishes" => "Bella"
+    ];
+
+    $tasks["Dusting"] = "Tera";
+    print_r($tasks);
+    // sort($tasks);    
+    //echo count($tasks);
+    //print_r($tasks);
+    //echo $tasks["Laundry"];
+    
+
     */
     ?>
     
